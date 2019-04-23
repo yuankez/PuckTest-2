e10453965fc1a5a9ba6fd53cf3d7f077308e2059
yuankez <34056855+yuankez@users.noreply.github.com>
2019-04-23 15:57:31 -0700

    Create node.js

---
+++ node.js
@@ -0,0 +1,27 @@
+<table border="1" cellpadding="3" id="printTable">
+    <tbody><tr>
+        <th>First Name</th>
+        <th>Last Name</th>      
+        <th>Points</th>
+    </tr>
+    <tr>
+        <td>Jill</td>
+        <td>Smith</td>      
+        <td>50</td>
+    </tr>
+    <tr>
+        <td>Eve</td>
+        <td>Jackson</td>        
+        <td>94</td>
+    </tr>
+    <tr>
+        <td>John</td>
+        <td>Doe</td>        
+        <td>80</td>
+    </tr>
+    <tr>
+        <td>Adam</td>
+        <td>Johnson</td>        
+        <td>67</td>
+    </tr>
+</tbody></table>
